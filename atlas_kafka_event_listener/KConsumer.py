bac7e14cb42eb853e2a12d656c1f62876c6c1134
pengfei <liu.pengfei@hotmail.fr>
2022-03-21 16:24:20 +0100

    refactor KafkaConsumer

--- atlas_kafka_event_listener/KConsumer.py
+++ atlas_kafka_event_listener/KConsumer.py
@@ -3,14 +3,15 @@ from typing import List
 from atlas_client.client import Atlas
 from kafka import KafkaConsumer
 
-from atlas_kafka_event_listener.LogManager import LogManager
+from atlas_kafka_event_listener import secret
 from atlas_kafka_event_listener.HiveEventHandler import HiveEventHandler
+from atlas_kafka_event_listener.LogManager import LogManager
 
 my_logger = LogManager(__name__).get_logger()
 my_logger.debug("Init kafka consumer")
 
 
-class KafkaConsumer:
+class KConsumer:
     def __init__(self, topic: str, group_id: str, broker_url: List[str], atlas_client: Atlas):
         self.topic = topic
         self.group_id = group_id
@@ -26,9 +27,11 @@ class KafkaConsumer:
         for message in consumer:
             # message value and key are raw bytes we need to deserialize it
             # in our case, we use decode('utf-8') to convert byte to string by using encoding 'utf-8'
+            event_key = message.key.decode('utf-8')
+            event_value = message.value.decode('utf-8')
             print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                                 message.offset, message.key.decode('utf-8'),
-                                                 message.value.decode('utf-8')))
+                                                 message.offset, event_key, event_value))
+            self.event_handler(event_key, event_value)
 
     def event_handler(self, event_key: str, event_value: str):
         if event_key == "create_table":
@@ -38,10 +41,26 @@ class KafkaConsumer:
 
 
 def main():
+    # create an atlas consumer instance
+    local = False
+    # config for atlas client
+    atlas_prod_hostname = "https://atlas.lab.sspcloud.fr"
+    atlas_prod_port = 443
+    atlas_local_hostname = "http://localhost"
+    login = "admin"
+    pwd = "admin"
+
+    if local:
+        atlas_client = Atlas(atlas_local_hostname, port=21000, username=login, password=pwd)
+    else:
+        # create an instance of the atlas Client with oidc token
+        atlas_client = Atlas(atlas_prod_hostname, atlas_prod_port, oidc_token=secret.oidc_token)
     topic = "hive_meta"
     group_id = "my-group"
     broker_url = ['localhost:9092']
-    start(topic, group_id, broker_url)
+
+    consumer = KConsumer(topic, group_id, broker_url, atlas_client=atlas_client)
+    consumer.start()
 
 
 if __name__ == "__main__":
