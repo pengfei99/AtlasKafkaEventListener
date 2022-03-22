99e0c67dca16706fb53e3a6f1c63b2373d6d2b48
pengfei <liu.pengfei@hotmail.fr>
2022-03-22 17:23:12 +0100

    add drop table event handler

--- atlas_kafka_event_listener/HiveEventHandler.py
+++ atlas_kafka_event_listener/HiveEventHandler.py
@@ -4,6 +4,7 @@ from atlas_client.client import Atlas
 from atlas_client.entity_management.hive.HiveColumnManager import HiveColumnManager
 from atlas_client.entity_management.hive.HiveDBManager import HiveDBManager
 from atlas_client.entity_management.hive.HiveTableManager import HiveTableManager
+from atlas_client.entity_search.EntityFinder import EntityFinder
 
 
 class HiveEventHandler:
@@ -12,6 +13,7 @@ class HiveEventHandler:
         self.hive_db_manager = HiveDBManager(atlas_client)
         self.hive_table_manager = HiveTableManager(atlas_client)
         self.hive_column_manager = HiveColumnManager(atlas_client)
+        self.finder=EntityFinder(atlas_client)
 
     def handle_create_table_event(self, event_msg: str):
         table_event_metadata = json.loads(event_msg)
@@ -22,14 +24,15 @@ class HiveEventHandler:
         cols = table_event_metadata["sd"]["cols"]
         data_location = table_event_metadata["sd"]["location"]
 
+        table_description = table_event_metadata["parameters"]["comment"]
+
         # step 1 create/update hive db
         # here cluster name is the name space of each user(e.g. KUBERNETES_NAMESPACE=user-pengfei)
-        cluster_name = "user-pengfei"
-        db_description = "database for my stock market, test toto"
+        cluster_name = table_event_metadata["clusterName"]
+        db_description = ""
         self.hive_db_manager.create_entity(db_name, cluster_name, db_description, owner=owner)
 
         # step 2 create/update table
-        table_description = "favorite stock, test toto"
         db_qualified_name = f"{cluster_name}@{db_name}"
         self.hive_table_manager.create_entity(table_name, db_qualified_name, table_description, owner=owner,
                                               create_time=create_time)
@@ -39,8 +42,14 @@ class HiveEventHandler:
         for col in cols:
             col_name = col["name"]
             col_type = col["type"]
-            col_description = "name of the stock, test toto"
+            col_description = col["comment"]
             self.hive_column_manager.create_entity(col_name, col_type, table_qualified_name,
                                                    col_description)
 
+    def handle_drop_table_event(self, event_msg: str):
+        table_event_metadata = json.loads(event_msg)
+        table_name = table_event_metadata['tableName']
+        db_name = table_event_metadata['dbName']
+        cluster_name = table_event_metadata["clusterName"]
+        self.finder.search_full_text()
 
