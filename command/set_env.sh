67a741cfaedb9ea8b63e572d9be8d3be1d9246aa
pengfei <liu.pengfei@hotmail.fr>
2022-05-17 17:20:20 +0200

    update command

--- command/set_env.sh
+++ command/set_env.sh
@@ -1,7 +1,11 @@
 export ATLAS_HOSTNAME=https://atlas.lab.sspcloud.fr
 export ATLAS_PORT=443
-export OIDC_TOKEN=
-export KAFKA_BROKER_URL=hadoop1.insee.fr:9092,hadoop2.insee.fr:9092,hadoop3.insee.fr:9092
+export KAFKA_BROKER_URL=kafka-0.kafka-headless:9092,kafka-1.kafka-headless:9092,kafka-2.kafka-headless:9092
 export KAFKA_TOPIC_NAME=hive-meta
 export CONSUMER_GROUP_ID=hive_atlas_meta
-export PYTHONPATH="${PYTHONPATH}:/path/to/AtlasKafkaEventListener"
+export KC_URL=https://auth.lab.sspcloud.fr/auth
+export REALM_NAME=sspcloud
+export CLIENT_ID=atlas-sa
+export CLIENT_SECRET=
+
+export PYTHONPATH="${PYTHONPATH}:/home/pliu/git/AtlasKafkaEventListener"
